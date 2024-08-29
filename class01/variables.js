7e89cf8a7006fdbba279a3cd685b39ebe940fe0b
Ayesha Rasheed <ayesharasheed.gc.039@gmail.com>
2024-08-28 23:26:40 -0700

    Add files via upload

---
+++ class01/variables.js
@@ -0,0 +1,5 @@
+const customerId = 123;
+ let customerName = "Ayesha";
+var customerAdress = "Layyah";
+var customerEmail= "ayesha@gmail.com";
+console.table([customerId, customerName, customerAdress, customerEmail]);
